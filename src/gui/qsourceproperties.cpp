a14f42c8b7f387f9d85cff4c43c8183c5e6c7fa8
chohner <mail@chohner.com>
2017-03-21 15:02:56 +0100

    cleanup

--- src/gui/qsourceproperties.cpp
+++ src/gui/qsourceproperties.cpp
@@ -343,7 +343,8 @@ void QSourceProperties::update_displays(const Source& source,
   {
     _properties_display->setText(source.properties_file.c_str());
   }
-this->raise();
+
+  this->raise();
 }
 
 void QSourceProperties::_expand()
