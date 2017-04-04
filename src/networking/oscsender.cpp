1d1e812af4272c985567bffe050ec9859879cfd6
David Runge <dave@sleepmap.de>
2017-04-04 23:31:12 +0200

    src/networking/oscsender.cpp: Adding reference to controller and handler to OscSender constructors. Adding start/stop functionality that subscribes/unsubscribes OscSender with/from the controller.

--- src/networking/oscsender.cpp
+++ src/networking/oscsender.cpp
@@ -2,18 +2,35 @@
 #include "oscsender.h"
 
 // client ctor
-ssr::OscSender::OscSender(OscHandler& handler, int port_out)
-  : _handler(handler)
+ssr::OscSender::OscSender(Publisher& controller, OscHandler& handler, int
+    port_out)
+  : _controller(controller)
+  , _handler(handler)
   , _send_from(lo::ServerThread(port_out))
+  , _is_subscribed(false)
 {}
 
 // server ctor
-ssr::OscSender::OscSender(OscHandler& handler, int port_out,
-    std::vector<lo::Address> client_addresses)
-  : _handler(handler)
+ssr::OscSender::OscSender(Publisher& controller, OscHandler& handler, int
+    port_out, std::vector<lo::Address> client_addresses)
+  : _controller(controller)
+  , _handler(handler)
   , _send_from(lo::ServerThread(port_out))
   , _client_addresses(client_addresses)
+  , _is_subscribed(false)
 {}
+
+ssr::OscSender::start()
+{
+  _controller.subscribe(*this);
+  _is_subscribed = true;
+}
+
+ssr::OscSender::stop()
+{
+  _controller.unsubscribe(*this);
+  _is_subscribed = false;
+}
 
 /** Implementation of function to return OscSender's _server_address
  * @return _server_address
