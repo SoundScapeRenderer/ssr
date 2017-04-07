cb87a801c1f237fb19fdca987881e7ade869d422
David Runge <dave@sleepmap.de>
2017-04-07 16:36:31 +0200

    src/networking/oscsender.cpp: Adding methods for sending OSC messages to all clients, server or specific client. Fixing all send methods to use a path std::string, where needed. Making all methods part of namespace ssr. Adding poll_all_clients method to send periodical '/poll' messages to all clients to make them subscribe to the server. Adding std::thread poll_thread to call poll_all_clients in a thread.

--- src/networking/oscsender.cpp
+++ src/networking/oscsender.cpp
@@ -3,6 +3,7 @@
  * @file oscsender.cpp
  */
 
+#include <chrono>
 #include "oschandler.h"
 #include "oscsender.h"
 
@@ -38,7 +39,9 @@ ssr::OscSender::OscSender(Publisher& controller, OscHandler& handler, int
   , _send_from(lo::ServerThread(port_out))
   , _client_addresses(client_addresses)
   , _is_subscribed(false)
+  , _poll_all_clients(false)
   , _mode(handler.mode())
+  , _poll_thread(0)
 {}
 
 /**
@@ -70,23 +73,47 @@ void ssr::OscSender::start()
   );
   VERBOSE("OSC URL: " << _send_from.url()<<".");
   _send_from.start();
+  _poll_all_clients = true;
+  _poll_thread = new std::thread(std::bind(&OscSender::poll_all_clients(), this);
 }
 
 /**
  * Function to stop the OscSender object
- * This unsubscribes the OscSender from the Publisher
+ * This unsubscribes the OscSender from the Publisher and stops the client
+ * polling thread
  */
 void ssr::OscSender::stop()
 {
   _controller.unsubscribe(*this);
   _is_subscribed = false;
+  VERBOSE2("Stopping client polling thread ...");
+  _poll_all_clients = false;
+  if (_poll_thread)
+  {
+    _poll_thread->join();
+  }
+  VERBOSE2("Client polling thread stopped.");
+}
+
+/**
+ * Sends a '/poll' message to all client instances listed in _client_addresses,
+ * then makes the thread calling this function sleep for 1000 milliseconds
+ */
+void ssr::OscSender::poll_all_clients()
+{
+  while(_poll_all_clients)
+  {
+    send_to_all_clients("poll", lo::Message());
+    //TODO find better solution to compensate for execution time
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
 }
 
 /**
  * Function to return OscSender's _server_address
  * @return a lo::Address object representing the current server for this client
  */
-lo::Address server_address()
+lo::Address ssr::OscSender::server_address()
 {
   return this->_server_address;
 }
@@ -95,38 +122,92 @@ lo::Address server_address()
  * Function to set OscSender's _server_address
  * @param server_address a lo::Address to be used as _server_address
  */
-void set_server_address(lo::Address server_address)
+void ssr::OscSender::set_server_address(lo::Address server_address)
 {
   this->_server_address = server_address;
 }
 
 /**
- * Function to send /poll OSC message to all clients
- */
-void poll_clients()
-{
-  for (const auto& client_address: _client_addresses)
-  {
-    client_address.send_from(_send_from, "poll");
-  }
-}
-
-/**
- * Function to send a lo::Message to the lo:Address setup as server
+ * Function to send a lo::Message to the lo:Address setup as server using a
+ * predefined path
+ * @param path a std::string defining the path to send to
  * @param message a predefined lo::Messge object to be sent
  */
-void send_to_server(lo::Message message)
+void ssr::OscSender::send_to_server(std::string path, lo::Message message)
 {
-  _server_address.send_from(_send_from, msg);
+  _server_address.send_from(_send_from, path, message);
 }
 
 /**
  * Function to send a lo::Bundle to the lo:Address setup as server
  * @param bundle a predefined lo::Bundle object to be sent
  */
-void send_to_server(lo::Bundle bundle)
+void ssr::OscSender::send_to_server(lo::Bundle bundle)
 {
   _server_address.send_from(_send_from, bundle);
 }
 
+/**
+ * Function to send a lo::Message to a client.
+ * @param address a lo:Address that will be sent to, when found in
+ * _client_addresses.
+ * @param path a std::string defining the path to send to
+ * @param message a predefined lo::Messge object to be sent
+ */
+void ssr::OscSender::send_to_client(lo::Address address, std::string path,
+    lo::Message message)
+{
+  for (const auto& client _client_addresses)
+  {
+    if(client.hostname() == address.hostname() && client.port() ==
+        address.port())
+    {
+      client.send_from(_send_from, path, message);
+    }
+  }
+}
 
+/**
+ * Function to send a lo::Bundle to a client.
+ * @param address a lo:Address that will be sent to, when found in
+ * _client_addresses.
+ * @param bundle a predefined lo::Bundle object to be sent
+ */
+void ssr::OscSender::send_to_client(lo::Address address, lo::Bundle bundle)
+{
+  for (const auto& client _client_addresses)
+  {
+    if(client.hostname() == address.hostname() && client.port() ==
+        address.port())
+    {
+      client.send_from(_send_from, bundle);
+    }
+  }
+}
+
+/**
+ * Function to send a lo::Message to all clients setup in _client_addresses
+ * vector.
+ * @param path a std::string defining the path to send to
+ * @param message a predefined lo::Messge object to be sent
+ */
+void ssr::OscSender::send_to_all_clients(std::string path, lo::Message message)
+{
+  for (const auto& client_address: _client_addresses)
+  {
+    client_address.send_from(_send_from, path, message);
+  }
+}
+
+/**
+ * Function to send a lo::Bundle to all clients setup in _client_addresses
+ * vector.
+ * @param bundle a predefined lo::Bundle object to be sent
+ */
+void ssr::OscSender::send_to_all_clients(lo::Bundle bundle)
+{
+  for (const auto& client_address: _client_addresses)
+  {
+    client_address.send_from(_send_from, bundle);
+  }
+}
