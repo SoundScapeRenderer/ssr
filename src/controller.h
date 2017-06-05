1e339cb7e184a6ab9212827a27cb7f7e70accc70
David Runge <dave@sleepmap.de>
2017-06-05 12:59:48 +0200

    src/controller.h: Wrapping call to Subscriber::set_source_orientation() in a call to Scene::get_source_orientation() first, to make the former not segfault, if a non-existent Source is supplied.

--- src/controller.h
+++ src/controller.h
@@ -1534,10 +1534,12 @@ void
 Controller<Renderer>::orient_source_toward_reference(const id_t id)
 {
   // take reference offset into account?
-  
-  _publish(&Subscriber::set_source_orientation, id
-    , (_scene.get_reference().position - 
-        *_scene.get_source_position(id)).orientation());
+  if (_scene.get_source_orientation(id))
+  {
+    _publish(&Subscriber::set_source_orientation, id
+      , (_scene.get_reference().position - 
+          *_scene.get_source_position(id)).orientation());
+  }
 }
 
 template<typename Renderer>
