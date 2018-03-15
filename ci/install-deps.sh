8c13ce723711662f0ea69f6ddbc14038530e7dad
David Runge <dave@sleepmap.de>
2018-03-15 23:18:16 +0100

    .travis.yml: Fixing qt4 installation on macOS. Moving installation and building of dependencies to separate packages.

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -2,12 +2,41 @@
 
 set -euo pipefail
 
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  # liblo-dev is still at 0.28 in trusty, but we require 0.29
-  wget https://downloads.sourceforge.net/liblo/liblo-0.29.tar.gz
-  tar xvf liblo-0.29.tar.gz && cd liblo-0.29
-  ./configure && make && sudo make install
-  sudo ldconfig
-  cd ..
+
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  # brew rightfully abandoned qt4
+  # https://github.com/cartr/homebrew-qt4
+  brew tap cartr/qt4
+  brew tap-pin cartr/qt4
+  brew install --c++11 asio \
+    autoconf \
+    doxygen \
+    ecasound \
+    fftw \
+    help2man \
+    jack \
+    libsndfile \
+    qt@4
 fi
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  # autotools, automake, make are present in the trusty image
+  sudo apt-get install -y \
+    g++ \
+    libasio-dev \
+    libqt4-dev \
+    libqt4-opengl-dev \
+    libecasoundc-dev \
+    ecasound \
+    libxml2-dev \
+    libfftw3-dev \
+    libsndfile1-dev \
+    libjack-dev \
+    libjack0 \
+    jackd1 \
+    pkg-config \
+    libtool
+fi
+
 exit 0
