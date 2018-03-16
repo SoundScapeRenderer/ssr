88870f577b4bc9db8e0fe24d4b4a4b9b1a7fdfd1
David Runge <dave@sleepmap.de>
2018-03-16 19:37:39 +0100

    ci/install-deps.sh: Adding conditional force installs for gcc-{6,7} and clang-3.8. .travis.yml: Adding linux os with gcc7 to matrix.

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -40,6 +40,18 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     jackd1 \
     pkg-config \
     libtool
+  # force installation of gcc-6 if required
+  if [ "${CC}" == "gcc-6" ]; then
+    sudo apt-get install gcc-6
+  fi
+  # force installation of gcc-7 if required
+  if [ "${CC}" == "gcc-7" ]; then
+    sudo apt-get install gcc-7
+  fi
+  # force installation of clang-3.8 if required
+  if [ "${CC}" == "clang-3.8" ]; then
+    sudo apt-get install clang-3.8
+  fi
 fi
 
 exit 0
