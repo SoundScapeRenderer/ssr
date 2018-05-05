d5581cca6f13b4342cfce2dfc8fa6bf87ec9dd2a
chohner <mail@chohner.com>
2018-05-05 20:30:27 +0200

    move all PKG_CONFIG_PATH logic into install-deps.sh

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -20,7 +20,7 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 
   # Add qt5 and openssl (asio dependency) to pkg config path
   if [ -z ${PKG_CONFIG_PATH+x} ]; then export PKG_CONFIG_PATH=''; fi  # creates empty $PKG_CONFIG_PATH if not set
-  export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/qt/lib/pkgconfig:$PKG_CONFIG_PATH
+  export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/qt/lib/pkgconfig:/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
 fi
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
