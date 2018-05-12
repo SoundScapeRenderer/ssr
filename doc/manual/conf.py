f1e49c60b54f9e85837cd579c534bfdc09833295
JensAhrens <jens.ahrens@chalmers.se>
2018-05-12 23:16:36 +0200

    DOC: add reference to MS Windows pre-release
    DOC: Update copyright year, add contributors

--- doc/manual/conf.py
+++ doc/manual/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SoundScape Renderer'
-copyright = u'2014, SSR Team'
+copyright = u'2018, SSR Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
