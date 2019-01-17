cbe10b7cb14b8c74388d04a134ad54f1e0f6fd18
Matthias Geier <Matthias.Geier@gmail.com>
2019-01-17 16:56:53 +0100

    TST: Update GCC, clang and Xcode versions on Travis-CI

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -22,7 +22,6 @@ fi
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   # autotools, automake, make are present in the trusty image
   sudo apt-get install -y \
-    g++ \
     libasio-dev \
     qt5-default \
     libecasoundc-dev \
@@ -37,18 +36,6 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     jackd1 \
     pkg-config \
     libtool
-  # force installation of gcc-6 if required
-  if [ "${CC}" == "gcc-6" ]; then
-    sudo apt-get install gcc-6 g++-6
-  fi
-  # force installation of gcc-7 if required
-  if [ "${CC}" == "gcc-7" ]; then
-    sudo apt-get install gcc-7 g++-7
-  fi
-  # force installation of clang-3.8 if required
-  if [ "${CC}" == "clang-3.8" ]; then
-    sudo apt-get install clang-3.8
-  fi
 
   # force qt 5
   export QT_SELECT=qt5
