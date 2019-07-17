e8554a4aac59507784518a2c4ed6c2d7a993b6ca
Matthias Geier <Matthias.Geier@gmail.com>
2019-07-17 09:47:26 +0200

    TST: Travis-CI update
    * Switch to bionic, update compiler versions
    * Install apt and brew packages with Travis addon
    * Remove "sudo"
    * Don't compile ASIO?
    * brew update
    * Use autogen.sh
    * Remove doxygen dependency

--- ci/build-deps.sh
+++ ci/build-deps.sh
@@ -18,19 +18,6 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   tar xvf liblo-0.29.tar.gz && cd liblo-0.29
   ./configure && make && sudo make install
   cd ..
-  if [ $COMPILE_ASIO -eq 1 ]; then
-    # remove previously installed asio, as we're trying a newer version
-    sudo apt-get remove libasio-dev
-    # compile and install asio 1.12.0
-    wget https://github.com/chriskohlhoff/asio/archive/asio-1-12-0.tar.gz
-    tar xvf asio-1-12-0.tar.gz
-    cd asio-asio-1-12-0/asio
-    autoreconf -vfi
-    ./configure --with-boost=no --prefix=/usr
-    make
-    sudo make install
-    cd ..
-  fi
 
   git clone https://github.com/vrpn/vrpn.git
   cd vrpn
