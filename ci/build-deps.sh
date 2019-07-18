ecbb4ab75c8e5e9f2848803c5117408fa60e7039
Matthias Geier <Matthias.Geier@gmail.com>
2019-07-18 17:38:14 +0200

    TST: Compile VRPN on Travis-CI (Linux only)

--- ci/build-deps.sh
+++ ci/build-deps.sh
@@ -33,4 +33,18 @@ sudo make install
 cd ..
 cd ..
 
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  git clone https://github.com/vrpn/vrpn.git
+  cd vrpn
+  mkdir build
+  cd build
+  cmake -DVRPN_BUILD_JAVA=OFF ..
+  make
+  sudo make install
+  cd ..
+  cd ..
+fi
+
+
 exit 0
