bf8d9d0b41ff38509dccb699f8d94e244dccfd67
Matthias Geier <Matthias.Geier@gmail.com>
2019-07-18 17:50:57 +0200

    TST: Download InterSense library on Travis-CI

--- ci/build-deps.sh
+++ ci/build-deps.sh
@@ -7,7 +7,6 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   wget https://downloads.sourceforge.net/liblo/liblo-0.29.tar.gz
   tar xvf liblo-0.29.tar.gz && cd liblo-0.29
   ./configure && make && sudo make install
-  sudo ldconfig
   cd ..
   if [ $COMPILE_ASIO -eq 1 ]; then
     # remove previously installed asio, as we're trying a newer version
@@ -20,7 +19,15 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     ./configure --with-boost=no --prefix=/usr
     make
     sudo make install
+    cd ..
   fi
+
+  wget https://www.intersense.com/wp-content/uploads/2018/12/InterSense_SDK_4.2381.zip
+  unzip InterSense_SDK_4.2381.zip
+  sudo cp SDK/Linux/Sample/*.h /usr/local/include
+  sudo cp SDK/Linux/x86_64/libisense.so /usr/local/lib
+
+  sudo ldconfig
 fi
 
 git clone git://github.com/zaphoyd/websocketpp.git
