9e2b019e436acb01c2d19ad25fd4a25328555c0d
Matthias Geier <Matthias.Geier@gmail.com>
2020-09-15 16:27:32 +0200

    CI: Move from Travis-CI to Github Actions

---
+++ ci/build-deps-ubuntu.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+git clone https://github.com/vrpn/vrpn.git
+cd vrpn
+mkdir build
+cd build
+cmake -DVRPN_BUILD_JAVA=OFF ..
+make
+sudo make install
+cd ..
+cd ..
+
+# libfmt >= 5 is needed, which is in Ubuntu disco
+git clone https://github.com/fmtlib/fmt.git
+cd fmt
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+cd ..
+cd ..
