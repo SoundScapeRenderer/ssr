ca44097861b9e3b7bb64a2556392ba92c6471780
Matthias Geier <Matthias.Geier@gmail.com>
2020-10-09 21:12:17 +0200

    Switch APF from subtree to submodule

--- apf/examples/startpd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-PD_PATCH=test_simpleprocessor.pd
-
-pd -path pd-linux/release-single "$PD_PATCH" &
--- apf/unit_tests/main.cpp
+++
@@ -1,9 +0,0 @@
-// This is the main file for all unit tests using CATCH.
-//
-// The tests are compiled and executed with "make", further tests can be
-// added in the Makefile.
-//
-// See also: https://github.com/philsquared/Catch/blob/master/docs/tutorial.md
-
-#define CATCH_CONFIG_MAIN
-#include "catch/catch.hpp"
