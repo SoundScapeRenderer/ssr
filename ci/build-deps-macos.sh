d7945c91bb756289fa049ef7e739944ae99a45f2
Matthias Geier <Matthias.Geier@gmail.com>
2022-12-10 20:02:52 +0100

    CI: Install websocketpp with brew

--- ci/build-deps-macos.sh
+++ ci/build-deps-macos.sh
@@ -2,16 +2,6 @@
 
 set -euo pipefail
 
-git clone https://github.com/zaphoyd/websocketpp.git
-cd websocketpp
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-cd ..
-cd ..
-
 git clone https://github.com/hoene/libmysofa.git
 cd libmysofa
 cd build
