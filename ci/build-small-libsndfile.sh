584c0dc5406b5bfc60669e49d3a2e200c3b3bf0c
Matthias Geier <Matthias.Geier@gmail.com>
2025-08-16 18:17:13 +0200

    libsndfile: cherry-pick fix for bool/true/false error

--- ci/build-small-libsndfile.sh
+++ ci/build-small-libsndfile.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 git clone https://github.com/libsndfile/libsndfile.git
 cd libsndfile
 git checkout 1.2.2
+# https://github.com/libsndfile/libsndfile/issues/1049
+git config user.name "Dummy Name for cherry-pick"
+git config --global user.email "cherry-pick@example.com"
+git cherry-pick 2251737b3b175925684ec0d37029ff4cb521d302
 autoreconf -vif
 ./configure \
   --disable-sqlite \
